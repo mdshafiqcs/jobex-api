77eaa3ad6f5bf9a6225da0ceefc7d11e6bd2c14d
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-24 18:33:19 +0600

    allApplications, updateStatus api moethods created for recruiter application route

--- controllers/application.controller.js
+++ controllers/application.controller.js
@@ -5,6 +5,111 @@ import mongoose, { isValidObjectId } from "mongoose";
 import { ApplicationStatusEnum } from "../constants.js";
 
 
+export const allApplications = asyncHandler(async(req,res) => {
+  const currentPage = req.query.page || 1;
+  const limit = req.query.limit || 10;
+  const jobId = req.params.jobId;
+
+  if(!isValidObjectId(jobId)){
+    throw new ApiError(400, "Invalid jobId format, can not parse to ObjectId")
+  }
+
+  const job = await Job.findById(jobId);
+
+  if(!job){
+    throw new ApiError(404, "Job not found");
+  }
+
+  const aggregate = Application.aggregate([
+    
+    {
+      $match: {
+        job: mongoose.Types.ObjectId.createFromHexString(jobId)
+      }
+    },
+
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'applicant',
+        foreignField: '_id',
+        as: "applicant",
+        pipeline: [
+          
+          {
+            $addFields: {
+              profilePhoto: "$profile.profilePhoto",
+              skills: "$profile.skills",
+              resume: "$profile.resume",
+              resumeOriginalName: "$profile.resumeOriginalName",
+            }
+          },
+
+          {
+            $project: {
+              _id: 1,
+              fullname: 1,
+              email: 1,
+              profilePhoto: 1,
+              skills: 1,
+              resume: 1,
+              resumeOriginalName: 1,
+            }
+          },
+        ]
+      }
+    },
+
+    {
+      $sort: {
+        createdAt: 1
+      }
+    }
+  ]);
+
+  const applications = await Application.aggregatePaginate(
+    aggregate, 
+    pagenateOption(currentPage, limit, "applications")
+  )
+
+  return res.status(200)
+  .json(
+    new ApiResponse(200, applications, "")
+  );
+
+})
+
+
+export const updateStatus = asyncHandler(async(req, res) => {
+  const {applicationId, status} = req.body;
+  if(!applicationId || !status){
+    throw new ApiError(400, "applicationId and status is required");
+  }
+
+  const validStatus = Object.values(ApplicationStatusEnum).some((value) => value === status.toLowerCase());
+
+  if(!validStatus){
+    throw new ApiError(400, `Please provide a valid status. [ ${Object.values(ApplicationStatusEnum)} ] these are the supported status `)
+  }
+
+  if(!isValidObjectId(applicationId)){
+    throw new ApiError(400, "Invalid applicationId, can not parse to ObjectId");
+  }
+
+  const application = await Application.findById(applicationId).populate("job","_id createdBy");
+
+  if(!application || application.job.createdBy.toString() !== req.user._id.toString()){
+    throw new ApiError(404, "Application not found");
+  }
+
+  application.status = status.toLowerCase();
+  await application.save();
+
+  return res.status(200).json(
+    new ApiResponse(200, null, "Application Status Updated Successfully!" )
+  )
+
+})
 
 export const applyJob = asyncHandler(async(req, res) => {
   const userId = req.user._id;
--- routes/recruiter/application.route.js
+++ routes/recruiter/application.route.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 
 // controller imports
+import { allApplications, updateStatus } from "../../controllers/application.controller.js"
+
 
 // middlewares
 import { recruiter } from "../../middlewares/index.js";
 
 const router = Router();
 
+router.route("/jobId=:jobId").get( recruiter, allApplications);
+router.route("/update").patch( recruiter, updateStatus);
 
 
 
