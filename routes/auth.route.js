c3bd5aae9c56466f8459368a090d16a9e7b9dc3c
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-24 15:38:25 +0600

    auth route created

---
+++ routes/auth.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { authValidator } from "../middlewares/validators/index.js"
+
+// controller imports
+
+
+const router = Router();
+
+// public routes
+
+
+export default router;
