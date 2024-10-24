148b1a3f53f00e9cd8974a4cb88a549c9d9ff21a
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-24 18:03:13 +0600

    ApplicationStatusEnum added

--- constants.js
+++ constants.js
@@ -2,3 +2,9 @@ export const UserRoleEnum = {
   jobseeker: "jobseeker",
   recruiter: "recruiter",
 }
+
+export const ApplicationStatusEnum = {
+  pending: "pending",
+  accepted: "accepted",
+  rejected: "rejected",
+}
