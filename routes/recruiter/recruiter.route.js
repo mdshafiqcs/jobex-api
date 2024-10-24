16ce3eb95e623bff08b51fd70ebc440624c8f32e
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-24 18:14:15 +0600

    getUser, logout and updateProfile api created for recruiter

--- routes/recruiter/recruiter.route.js
+++ routes/recruiter/recruiter.route.js
@@ -2,12 +2,22 @@ import { Router } from "express";
 import { authValidator } from "../../middlewares/validators/index.js"
 
 // controller imports
+import { getUser, updateProfile, logout } from "../../controllers/user.controller.js"
 
 
 // middlewares
+import { recruiter, upload } from "../../middlewares/index.js";
+
 
 
 const router = Router();
+
+
+router.route("/").get(recruiter, getUser);
+router.route("/logout").get(recruiter, logout);
+
+router.route("/update-profile").post( upload.fields([ {name: "profilePhoto", maxCount: 1}]), authValidator.updateProfile, recruiter, updateProfile);
+
 
 
 
