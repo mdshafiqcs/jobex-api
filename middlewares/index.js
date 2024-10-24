0b7d6598450d09760d43c0b7661eb9d80d18822f
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-24 16:01:39 +0600

    upload middleware created

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 import errorHandler from "./errorHandler.middleware.js";
 import jobseeker from "./jobseeker.middleware.js";
+import { upload } from "./multer.middleware.js";
 
 
 export {
   errorHandler,
   jobseeker,
+  upload,
 
 }
